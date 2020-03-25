416737d7c1833167997e56b0c843a14daa0145ac
waheealam <walam@unomaha.edu>
2020-03-25 15:58:34 -0500

    Created Game Logic

--- untitled folder/memory-game/js/main.js
+++ untitled folder/memory-game/js/main.js
@@ -1,16 +1,49 @@
 const cards=["queen","queen","king","king"];
 let cardsInPlay=[];
-let cardOne=cards[0];
-cardsInPlay.push(cardOne);
-console.log("User filpped Queen");
-let cardTwo=cards[1];
-cardsInPlay.push(cardTwo);
-console.log("User filpped king");
-if(cardsInPlay.length===2)
+function checkForMatch()
 {
 	if(cardsInPlay[0]===cardsInPlay[1])
-		alert("You found a match!");
-	else
-		alert("Sorry Try again");
+	{
+		console.log(" You found a Match !");
+	}
+		else
+		{
+			console.log("Sorry Try Again");
+		}
+	
+};
 
-}
+function flipCard(cardId)
+{
+
+console.log("User flipped "+ cards[cardId]);
+cardsInPlay.push(cards[cardId]);
+
+};
+
+flipCard(0);
+flipCard(1);
+checkForMatch();
+
+// if(cardsInPlay.length===2)
+// {
+// 	if(cardsInPlay[0]===cardsInPlay[1])
+// 		alert("You found a match!");
+// 	else
+// 		alert("Sorry Try again");
+// }
+
+// let cardOne=cards[0];
+// cardsInPlay.push(cardOne);
+// console.log("User filpped Queen");
+// let cardTwo=cards[2];
+// cardsInPlay.push(cardTwo);
+// console.log("User filpped king");
+// if(cardsInPlay.length===2)
+// {
+// 	if(cardsInPlay[0]===cardsInPlay[1])
+// 		alert("You found a match!");
+// 	else
+// 		alert("Sorry Try again");
+
+// }
